74ae394875aee68a1314f03f267af92fd50e7b29
kipris <tnzaria@gmail.com>
2021-08-13 11:58:33 +0300

    update mobile view

--- js/index.js
+++ js/index.js
@@ -1,12 +1,35 @@
 window.onload = function () {
 
 	// scroll down
-	let element = document.getElementById('why-choose');
 	let btn = document.getElementById('scroll-down');
+	let element = document.getElementById('why-choose');
+
+	function scrollToSmoothly(pos, time) {
+    var currentPos = window.pageYOffset;
+    var start = null;
+    if(time == null) time = 500;
+    pos = +pos, time = +time;
+    window.requestAnimationFrame(function step(currentTime) {
+        start = !start ? currentTime : start;
+        var progress = currentTime - start;
+        if (currentPos < pos) {
+            window.scrollTo(0, ((pos - currentPos) * progress / time) + currentPos);
+        } else {
+            window.scrollTo(0, currentPos - ((currentPos - pos) * progress / time));
+        }
+        if (progress < time) {
+            window.requestAnimationFrame(step);
+        } else {
+            window.scrollTo(0, pos);
+        }
+    });
+	}
 
 	function handleButtonClick() {
-	  element.scrollIntoView({block: 'start', behavior: 'smooth'});
+		scrollToSmoothly(element.getBoundingClientRect().top, 800);
 	}
 
 	btn.addEventListener('click', handleButtonClick);
+
+	
 }
