1deb3779a37674fb87e0254d440fa27a7288c360
kipris <tnzaria@gmail.com>
2021-08-13 17:38:11 +0300

    desktop view

--- js/index.js
+++ js/index.js
@@ -1,35 +1,69 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", () => {
+
+	// google analytics
+	const cta = document.getElementById('button');
+	cta.addEventListener('click', function () {
+		const source = this.closest('section').id;
+		document.head.append(`<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			ga('create', 'GTM-P5NWMTS', 'auto');
+			ga('send', '${source} block');</script>`);
+	});
 
 	// scroll down
-	let btn = document.getElementById('scroll-down');
-	let element = document.getElementById('why-choose');
+	window.__forceSmoothScrollPolyfill__ = true;
+	const btnDown = document.getElementById('scroll-down');
+	const element = document.getElementById('why-choose');
 
-	function scrollToSmoothly(pos, time) {
-    var currentPos = window.pageYOffset;
-    var start = null;
-    if(time == null) time = 500;
-    pos = +pos, time = +time;
-    window.requestAnimationFrame(function step(currentTime) {
-        start = !start ? currentTime : start;
-        var progress = currentTime - start;
-        if (currentPos < pos) {
-            window.scrollTo(0, ((pos - currentPos) * progress / time) + currentPos);
-        } else {
-            window.scrollTo(0, currentPos - ((currentPos - pos) * progress / time));
-        }
-        if (progress < time) {
-            window.requestAnimationFrame(step);
-        } else {
-            window.scrollTo(0, pos);
-        }
-    });
+	function handleScrollDown() {
+		element.scrollIntoView({ block: 'start', behavior: 'smooth' });
 	}
 
-	function handleButtonClick() {
-		scrollToSmoothly(element.getBoundingClientRect().top, 800);
+	btnDown.addEventListener('click', handleScrollDown);
+
+	// sticky button
+	const target = document.querySelector('.main-cta');
+	const bottomElement = document.getElementById('proposal');
+	window.addEventListener('scroll', function () {
+		let currentPos = window.scrollY;
+		let reached = currentPos >= bottomElement.getBoundingClientRect().top;
+		if (currentPos >= element.getBoundingClientRect().top && !reached) {
+			target.classList.add('sticky');
+		} else {
+			target.classList.remove('sticky');
+		}
+	});
+
+	// gif lazy load
+	const images = Array.from(document.querySelectorAll('.lazy-image img'));
+
+	if ('IntersectionObserver' in window) {
+		const imageObserver = new IntersectionObserver((entries, observer) => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					const image = entry.target;
+
+					image.src = image.dataset.src;
+					image.onload = () => image.previousElementSibling.remove();
+
+					imageObserver.unobserve(image);
+				}
+			});
+		});
+
+		images.forEach(img => imageObserver.observe(img));
 	}
 
-	btn.addEventListener('click', handleButtonClick);
-
+	// toggle why-choose
+	if (window.innerWidth <= 767) {
+		const reasons = Array.from(document.querySelectorAll('.reason'));
+		reasons.forEach(reason => {
+			reason.addEventListener('click', () => {
+				reason.classList.toggle('closed')
+			})
+		})
+	}
 	
-}
+})
